81649020a4b5b7ba13b6109a8600a987fbfae97c
Emile-Nsengimana <emilereas7@gmail.com>
2020-09-06 18:44:14 +0200

    ft(booking): client booking process
     - book hotel, car or destination
    
    [finishes #174470912]

--- src/components/supplier/SupplierItems.js
+++ src/components/supplier/SupplierItems.js
@@ -1,100 +1,152 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Fab from "@material-ui/core/Fab";
+import { Checkbox } from "@material-ui/core";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
 
-const useStyles = makeStyles(theme => ({
-	cardGrid: {
-		paddingTop: theme.spacing(16),
-		paddingBottom: theme.spacing(2),
-		backgroundColor: '#f2f6fb',
-		marginBottom: 40,
-		borderRadius: '10px',
-	},
-	card: {
-		height: '100%',
-		display: 'flex',
-		flexDirection: 'column',
-		paddingBottom: theme.spacing(2),
-		'&:hover': {
-			background: '#e3e5e6',
-		},
-	},
-	cardMedia: {
-		paddingTop: '56.25%', // 16:9
-	},
-	cardContent: {
-		flexGrow: 1,
-	},
-	footer: {
-		backgroundColor: theme.palette.background.paper,
-		padding: theme.spacing(6),
-	},
-	titleOrganization: {
-		marginTop: -50,
-	},
-	areaStyle: {
-		marginTop: -30,
-		marginLeft: 25,
-	},
+const useStyles = makeStyles((theme) => ({
+  titleOrganization: {
+    marginTop: "50px",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: "10px",
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    objectFit: "cover",
+  },
+  itemBox: {
+    boxShadow: "0 2px 3px 0 #ccc",
+    margin: "20px 0 0 0px",
+    width: "90%",
+    padding: "10px",
+    backgroundColor: "white",
+    borderRadius: "8px",
+  },
 }));
 
-const SupplierItems = props => {
-	const classes = useStyles();
-	return (
-		<Container className={classes.cardGrid} maxWidth='lg'>
-			<Typography
-				component='h3'
-				variant='h5'
-				align='center'
-				color='textPrimary'
-				className={classes.titleOrganization}
-				gutterBottom
-			>
-				Availability
-			</Typography>
-			<Grid container spacing={4}>
-				{props.items &&
-					props.items.map(item => (
-						<Grid item key={item.id} xs={12} sm={6} md={3}>
-							<Card className={classes.card} elevation={3}>
-								<CardMedia
-									className={classes.cardMedia}
-									image={item.itemImage}
-									title={item.itemName}
-								/>
-								<CardContent className={classes.cardContent}>
-									<Typography gutterBottom variant='h5' component='h2'>
-										{item.itemName}
-									</Typography>
-									<Typography>{item.itemDescription}</Typography>
-								</CardContent>
-								<CardActions>
-									<Button
-										variant='contained'
-										size='large'
-										color='primary'
-										disabled
-									>
-										<Typography variant='h5' component='h6' color='primary'>
-											{item.itemPrice} Frw
-										</Typography>
-									</Button>
-									<Button variant='contained' size='large' color='secondary'>
-										Book
-									</Button>
-								</CardActions>
-							</Card>
-						</Grid>
-					))}
-			</Grid>
-		</Container>
-	);
+const SupplierItems = (props) => {
+  const classes = useStyles();
+  const bookings = JSON.parse(localStorage.getItem("bookingSummary"));
+  const [bookingSummary, setBookingSummary] = useState(
+    bookings !== null ? bookings : []
+  );
+
+  const handleAddItem = (e, item) => {
+    console.log(e.target.firstChild);
+    const tempTot = (localStorage.getItem("totalPrice") || 0) * 1;
+    const tot = tempTot + item["itemPrice"] * 1;
+    localStorage.setItem("totalPrice", tot);
+
+    const { id, itemName, itemPrice } = item;
+    const bookItem = {
+      id,
+      itemName,
+      itemPrice: itemPrice * 1,
+    };
+    const booked = bookingSummary.find((bk) => bk.id === item.id);
+    let updatedBooking = [...bookingSummary, bookItem];
+    if (booked) {
+      updatedBooking = bookingSummary.filter((bk) => bk.id !== item.id);
+      const updatedTotPrice = tempTot - item["itemPrice"] * 1;
+      localStorage.setItem("totalPrice", updatedTotPrice);
+    }
+    setBookingSummary(updatedBooking);
+    localStorage.setItem("bookingSummary", JSON.stringify(updatedBooking));
+  };
+
+  return (
+    <div>
+      <Typography
+        component="h3"
+        variant="h5"
+        align="left"
+        color="textPrimary"
+        className={classes.titleOrganization}
+        gutterBottom
+      >
+        Availability
+      </Typography>
+
+      <div>
+        {props.items &&
+          props.items.map((item) => (
+            <Grid
+              key={item.itemName}
+              container
+              spacing={3}
+              item
+              xs={12}
+              sm={8}
+              md={8}
+              className={classes.itemBox}
+            >
+              <Grid container spacing={3}>
+                <Grid item xs={4} sm={2} md={2}>
+                  <ButtonBase className={classes.image}>
+                    <img
+                      className={classes.img}
+                      alt="..."
+                      src={item.itemImage}
+                    />
+                  </ButtonBase>
+                </Grid>
+                <Grid item xs={12} sm={9} md={9} style={{ display: "flex" }}>
+                  <Grid item xs container direction="column" spacing={2}>
+                    <Grid item xs>
+                      <Typography gutterBottom variant="subtitle1">
+                        {item.itemName}
+                      </Typography>
+                      <Typography variant="body2" gutterBottom>
+                        {item.itemDescription}
+                      </Typography>
+                    </Grid>
+
+                    <FormControl component="fieldset">
+                      <FormGroup>
+                        <FormControlLabel
+                          value="Select"
+                          control={
+                            <Checkbox
+                              color="primary"
+                              onClick={(e) => handleAddItem(e, item)}
+                            />
+                          }
+                          label="Select"
+                          labelPlacement="end"
+                        />
+                      </FormGroup>
+                    </FormControl>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="subtitle1">
+                      <Fab
+                        variant="extended"
+                        size="small"
+                        color="primary"
+                        aria-label="add"
+                        className={classes.btnBooking}
+                      >
+                        {item.itemPrice + "Rwf/Per night"}
+                      </Fab>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          ))}
+      </div>
+    </div>
+  );
 };
 export default SupplierItems;
--- src/redux/reducers/clientReducer.js
+++ src/redux/reducers/clientReducer.js
@@ -1,51 +1,58 @@
 import {
-	GET_HOTELS_SUCCESS,
-	GET_HOTELS_FAILURE,
-	GET_TOURS_SUCCESS,
-	GET_TOURS_FAILURE,
-	GET_CARS_SUCCESS,
-	GET_CARS_FAILURE,
-} from '../types';
+  GET_HOTELS_SUCCESS,
+  GET_HOTELS_FAILURE,
+  GET_TOURS_SUCCESS,
+  GET_TOURS_FAILURE,
+  GET_CARS_SUCCESS,
+  GET_CARS_FAILURE,
+  BOOKING,
+} from "../types";
 
 const initialState = {
-	hotels: {},
-	tours: {},
-	cars: {},
+  hotels: {},
+  tours: {},
+  cars: {},
+  bookedItems: [],
 };
 
 export default function (state = initialState, action) {
-	switch (action.type) {
-		case GET_HOTELS_SUCCESS:
-			return {
-				...state,
-				hotels: action.payload,
-			};
-		case GET_HOTELS_FAILURE:
-			return {
-				...state,
-				hotels: action.payload,
-			};
-		case GET_TOURS_SUCCESS:
-			return {
-				...state,
-				tours: action.payload,
-			};
-		case GET_TOURS_FAILURE:
-			return {
-				...state,
-				tours: action.payload,
-			};
-		case GET_CARS_SUCCESS:
-			return {
-				...state,
-				cars: action.payload,
-			};
-		case GET_CARS_FAILURE:
-			return {
-				...state,
-				cars: action.payload,
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case GET_HOTELS_SUCCESS:
+      return {
+        ...state,
+        hotels: action.payload,
+      };
+    case GET_HOTELS_FAILURE:
+      return {
+        ...state,
+        hotels: action.payload,
+      };
+    case GET_TOURS_SUCCESS:
+      return {
+        ...state,
+        tours: action.payload,
+      };
+    case GET_TOURS_FAILURE:
+      return {
+        ...state,
+        tours: action.payload,
+      };
+    case GET_CARS_SUCCESS:
+      return {
+        ...state,
+        cars: action.payload,
+      };
+    case GET_CARS_FAILURE:
+      return {
+        ...state,
+        cars: action.payload,
+      };
+    case BOOKING:
+      return {
+        ...state,
+        bookedItems: [...state.bookedItems, ...action.payload],
+      };
+    default:
+      return state;
+  }
 }
