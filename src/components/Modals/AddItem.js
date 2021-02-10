54ff025dc78c929bb7c5f3b0b0600ef4a193ebb1
Celestin NIYONSABA <niyoceles3@gmail.com>
2021-02-10 19:26:32 +0200

    updated headers

--- src/components/Modals/AddItem.js
+++ src/components/Modals/AddItem.js
@@ -106,12 +106,8 @@ const AddItem = () => {
 		data.append('timestamp', (Date.now() / 1000) | 0);
 		data.append('folder', 'QUINCAPARADI/ITEMS');
 
-		const headers = {
-			'Content-Type': 'application/json',
-		};
-
 		const options = {
-			headers: headers,
+			headers: { 'X-Requested-With': 'XMLHttpRequest' },
 			onUploadProgress: progressEvent => {
 				const { loaded, total } = progressEvent;
 				let percent = Math.floor((loaded * 100) / total);
@@ -119,15 +115,10 @@ const AddItem = () => {
 			},
 		};
 
-
-
 		axios
 			.post(
 				`https://api.cloudinary.com/v1_1/${REACT_APP_CLOUDINARY_NAME}/image/upload`,
 				data,
-				{
-					'Access-Control-Allow-Origin': '*',
-				},
 				options
 			)
 			.then(res => {
